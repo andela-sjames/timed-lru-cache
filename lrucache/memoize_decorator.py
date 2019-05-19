cf6565a17c2a9c6ce7f5445651f0a49d82b822b4
andela-sjames <samuel.james@andela.com>
2019-05-19 13:30:23 +0100

    Add decorator for memoization

---
+++ lrucache/memoize_decorator.py
@@ -0,0 +1,42 @@
+from types import MethodType
+
+from utils.cache_decorator import Decoratorfunc
+
+class MemoizedCache(Decoratorfunc):
+    
+    def __init__(self, fn, cache, timeout = None):
+        self.fn = fn
+        self.cache = cache
+        self.hits = 0
+        self.misses = 0
+    
+    def reset(self):
+        self.hits = 0
+        self.misses = 0
+    
+    def cache_info(self):
+        cache_info = f'{self.__class__.__name__}(hits={self.hits}, misses={self.misses}, maxsize={self.cache.maxSize}, currsize={self.cache.currentSize})'
+        print(cache_info)
+
+    def __call__(self, *args, **kwargs):
+
+        key = self._convert_call_arguments_to_hash(args, kwargs)
+
+        result = self.cache[key]
+        if result is None:
+            # We have an entry not in the cache
+            self.misses += 1
+            result = self.fn(*args, **kwargs)
+            self.cache[key] = result
+        else:
+            self.hits += 1
+
+        return result
+
+    def __get__(self, instance, cls):
+        # return a method if it is called on an instance
+        return self if instance is None else MethodType(self, instance)
+
+    @staticmethod
+    def _convert_call_arguments_to_hash(args, kwargs):
+        return hash(str(args) + str(kwargs))
