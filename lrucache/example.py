9003020479841838909cc0513405c510acb3a023
andela-sjames <samuel.james@andela.com>
2019-05-20 11:06:54 +0100

    Update readme.

--- lrucache/example.py
+++ lrucache/example.py
@@ -39,22 +39,23 @@ print(get_random(5)) # 0.5001657308606899
 print(get_random(5)) # 0.5001657308606899
 print(get_random(7)) # 0.7652871914811611
 print(get_random(8)) # 0.4366852743803227
+print(get_random(1)) # 0.6811475789638526
 
 print(get_random.misses)
-# => 6
+# => 7
 print(get_random.hits)
 # => 2
 
 # cache info
 get_random.cache_info()
-# => MemoizedCache(hits=2, misses=6, maxsize=5, currsize=5)
+# => MemoizedCache(hits=2, misses=7, maxsize=5, currsize=5)
 
 # Simple report on performance
 report = f'Hit %: {(float(get_random.hits) / (get_random.hits + get_random.misses))}'
 print(report)
-# => Hit %: 0.25
+# => Hit %: 0.2222222222222222
 
-# Test time constraint, this is still buggy :sweat smile:
+# Test time constraint
 timed_lru = LRUCache(maxSize = 4, timeout=10)
 
 timed_lru["a"] = 202
